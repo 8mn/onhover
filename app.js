2e6f4d363a32a742af896ed0a6dbcba6ff489aba
calmighted <calmighted@gmail.com>
2021-03-27 12:09:02 +0530

    added mouseover

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 document.body.style.border = "5px solid red";
-console.log("fuck you!")
+console.log("**********************")
 let imageLink = document.querySelectorAll('a');
 
 // let anchor = imageLink.getAttribute('rel')
 // let anchor = document.imageLink.childNodes;
 
 // console.log(anchor)
-console.log(imageLink[10])
+// console.log(imageLink[10])
+let image = document.createElement('img')
+
+let src = ''
+
+image.style.display = 'none'
+image.style.margin = 'auto'
+image.setAttribute('src') = src
+
 
 
 for(let i=0;i< imageLink.length;i++){
     if(imageLink[i].getAttribute('rel') === "noopener nofollow ugc"){
-        console.log(imageLink[i])
-    }else{
-        console.log("Nope")
+        // console.log(imageLink[i])
+        if(imageLink[i].getAttribute('href').includes('.jpg')){
+            // console.log("#################")
+            imageLink[i].onmouseover = function(){
+                alert("hii")
+                src = imageLink.getAttribute('href')
+                image.style.display = 'block'
+            }
+            // console.log(imageLink[i])
+        }else if(imageLink[i].getAttribute('href').includes('.png')){
+            imageLink[i].onmouseover = function(){
+                alert("hii")
+                src = imageLink.getAttribute('href')
+                image.style.display = 'block'
+            }
+        }
     }
 }
 
